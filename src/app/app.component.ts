53b4bea2741804637091684ef644e74d8c3ff3ea
nyabeu <onyabeu@gmail.com>
2022-06-22 15:15:07 +0200

    assignment2 the basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assignment2';
+  userName: string = "";
+
+
+  OnReset(){
+    this.userName = "";
+  }
 }
